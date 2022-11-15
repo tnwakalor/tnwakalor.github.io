b841aa7d9b3ce6f5e8b1a1b95ba8984df65e4b0f
Tochukwu Nwakalor <111817795+tnwakalor@users.noreply.github.com>
2022-11-14 19:39:27 -0500

    New website updates

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -1,16 +1,38 @@
 function scriptTest(){
-     var name = document.getElementById("full-name").innerHTML;
-     document.getElementById("demo").innerHTML = "Hello, " + name;
-     
+     var name = document.getElementById("full-name").value;
+     var feeling = document.getElementById("feeling").value;
+     document.getElementById("demo").innerHTML = "Hello, " + name + "!" + " Tochway Consulting welcomes you to our website! We're glad you're doing " + feeling;
+    
 }
-
-var dt = new Date();
 
 
 function refreshTime() {
-    const timeDisplay = document.getElementById("date-time");
-    const dateString = new Date().toLocaleString();
-    const formattedString = dateString.replace(", ", " - ");
-    timeDisplay.textContent = formattedString;
-  }
-  setInterval(refreshTime, 1000);
+     const timeDisplay = document.getElementById("date-time");
+     const dateString = new Date().toLocaleString();
+     const formattedString = dateString.replace(", ", " - ");
+     timeDisplay.textContent = formattedString;
+   }
+   setInterval(refreshTime, 1000);
+
+
+function calculatePets(){
+     var firstNum = parseInt(prompt("Quick! Enter one of your favorite numbers!"));
+     var secondNum = parseInt(prompt("Enter another one!"));
+
+     
+     totalPets = firstNum + secondNum;
+     alert("You can only have " + totalPets + " Pets!");
+     
+}
+
+function convertTemp(){
+     var celsius = parseInt(prompt("Enter your temperature value"))
+  
+     var farenheit = (celsius * 1.8) + 32;
+
+     alert(celsius + " celsius is " + farenheit + " farenheit!");
+}
+
+function generatePolygon(){
+     
+}
