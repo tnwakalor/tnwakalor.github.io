e846054c497238bce99b621b156bb8a24f200c6c
Tochukwu Nwakalor <111817795+tnwakalor@users.noreply.github.com>
2022-12-15 14:55:13 -0500

    new stuff

---
+++ itis3135/scripts/activity10.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+    
+
+        // set up event handlers for links
+        
+        $("#image_list a").click(function (evt) {
+        
+        // cancel the default action of the link
+        
+        evt.preventDefault();
+        
+        var imageURL = $(this).attr("href");
+        
+        var caption = $(this).attr("title");
+        
+        $("#caption").fadeOut(1000, function(){
+        
+        $("#caption").text(caption).fadeIn(1000);
+        
+        });
+        
+        $("#image").fadeOut(1000, function(){
+        
+        $("#image").attr("src", imageURL).fadeIn(1000);
+        
+        });
+        
+        }); // end click
+        
+        // move focus to first thumbnail
+        
+        $("li:first-child a").focus();
+        
+        $("#image").hide().fadeIn(1000);
+        
+        $("#caption").hide().fadeIn(1000);
+        
+        }); // end ready
--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -44,3 +44,120 @@ function multiplyNum(){
 
      document.getElementById("demo-two").innerHTML = "We will purchase " + product + " " + rando + "!"; 
 }
+
+
+$(document).ready(function() {
+     // Select the slides container
+     var slides = $('#slideshow .slides');
+     
+     // Set the initial slide index to 0
+     var currentSlide = 0;
+     
+     // Set the initial CSS properties for the slides container
+     slides.css({
+       'width': '300px',
+       'height': '200px',
+       'overflow': 'hidden',
+       
+     });
+     
+     // Select the slides and set their initial CSS properties
+     slides.find('img').css({
+       'width': '300px',
+       'height': '200px',
+       'float': 'left'
+     }); 
+
+
+     // Function to show the next slide
+  function nextSlide() {
+     // Increment the current slide index
+     currentSlide++;
+     
+     // If the current slide index is greater than the total number of slides, set it back to 0
+     if (currentSlide > slides.find('img').length - 1) {
+       currentSlide = 0;
+     }
+     
+     // Animate the slides container to the left by the width of one slide
+     slides.animate({
+       'margin-left': '-=300px'
+     }, 500);
+}
+
+   // Function to show the previous slide
+   function prevSlide() {
+     // Decrement the current slide index
+     currentSlide--;
+     
+     // If the current slide index is less than 0, set it to the last slide
+     if (currentSlide < 0) {
+       currentSlide = slides.find('img').length - 1;
+     }
+     
+     // Animate the slides container to the right by the width of one slide
+     slides.animate({
+       'margin-left': '+=300px'
+     }, 500);
+   
+     }})
+     
+
+$(document).ready(function() {
+     // Select all of the images on the page
+     var images = $('img');
+     
+     // Attach a click event listener to each image
+     images.on('click', function() {
+       // Get the source URL of the image that was clicked
+       var src = $(this).attr('src');
+       
+       // Create an overlay element to display the image
+       var overlay = $('<div id="overlay"></div>');
+       
+       // Set the CSS for the overlay element
+       overlay.css({
+         'position': 'absolute',
+         'top': '0',
+         'left': '0',
+         'width': '100%',
+         'height': '100%',
+         'background': 'rgba(0, 0, 0, 0.7)',
+         'z-index': '10'
+       });
+       
+       // Create an image element to show the full-sized image
+       var image = $('<img>');
+       
+       // Set the source URL for the image element
+       image.attr('src', src);
+       
+       // Set the CSS for the image element
+       image.css({
+         'position': 'absolute',
+         'top': '50%',
+         'left': '50%',
+         'transform': 'translate(-50%, -50%)',
+         'max-width': '90%',
+         'max-height': '90%'
+       });
+       
+       // Append the image to the overlay element
+       overlay.append(image);
+       
+       // Append the overlay to the body of the page
+       $('body').append(overlay);
+     });
+     
+     // Attach a click event listener to the overlay element to close it when it is clicked
+     $(document).on('click', '#overlay', function() {
+       $(this).remove();
+     });
+   });
+   
+
+   
+ 
+// create event handlers for the next and previous buttons
+$("#next").click(nextSlide);
+$("#previous").click(prevSlide);
