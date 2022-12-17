3c8afacd3c7045c95c5803e46398d8fc4f1aed4d
Tochukwu Nwakalor <111817795+tnwakalor@users.noreply.github.com>
2022-12-16 22:18:45 -0500

    New Gallery Page

--- itis3135/clientproject/scripts/client.js
+++ itis3135/clientproject/scripts/client.js
@@ -4,3 +4,10 @@ function inform(){
   document.getElementById("submit").innerHTML = "Your response has been recorded!"
   
 }
+
+$(document).ready(function() {
+  $("#accordion").accordion({
+      active: false,
+      collapsible: true
+  });
+});
